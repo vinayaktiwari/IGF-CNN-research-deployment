486d1c46f5368bfa2a18d7901d58c5d5b90dfee3
Vinayak Tiwari <GREYORANGE\vinayak.t@ggn002033.greyorange.com>
2023-07-22 21:19:09 +0530

    setup

--- templates.py
+++ templates.py
@@ -5,7 +5,7 @@ from pathlib import Path
 logging.basicConfig(level =logging.INFO,
                      format='[%(asctime)s]: %(message)s:')
 
-project_name = "cnnClassifier"
+project_name = "IgfCnnClassifier"
 
 
 list_of_files = [
