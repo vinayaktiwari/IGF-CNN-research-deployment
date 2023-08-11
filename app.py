bf16909a11d61aef1f287642e411c1f6743a59d8
Vinayak Tiwari <GREYORANGE\vinayak.t@ggn002033.greyorange.com>
2023-08-11 20:37:06 +0530

    added github runner3

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     # app.run(host='0.0.0.0', port=8082) #local host
-    app.run(host='0.0.0.0', port=8080) #for AWS
+    app.run(host='0.0.0.0', port=8081) #for AWS
     # app.run(host='0.0.0.0', port=80) #for AZURE
 
