24d8b85ad2b22703a39e7c5020313011380004c5
Vinayak Tiwari <GREYORANGE\vinayak.t@ggn002033.greyorange.com>
2023-08-11 14:20:55 +0530

    github actions and dockerfile added2

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8082) #local host
-    # app.run(host='0.0.0.0', port=8080) #for AWS
+    # app.run(host='0.0.0.0', port=8082) #local host
+    app.run(host='0.0.0.0', port=8080) #for AWS
     # app.run(host='0.0.0.0', port=80) #for AZURE
 
